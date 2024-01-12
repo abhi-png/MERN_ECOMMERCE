1f46328effaff0fbc9ab8fb6373fae9f0cae0158
Abinash Kar <abinash.kar611@gmail.com>
2024-01-12 21:27:49 +0530

    New 02

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Product from "./customer/components/Product/Product";
 import ProductDetails from "./customer/components/ProductDetails/ProductDetails";
 import Cart from "./customer/components/Cart/Cart";
 import Checkout from "./customer/components/Checkout/Checkout";
+import Order from "./customer/components/Order/Order";
 import Footer from "./customer/components/Footer/Footer";
 
 const App = () => {
@@ -15,7 +16,8 @@ const App = () => {
             {/* <Product /> */}
             {/* <ProductDetails /> */}
             {/* <Cart /> */}
-            <Checkout />
+            {/* <Checkout /> */}
+            <Order />
             <Footer />
         </div>
     )
---
+++ src/customer/components/Order/OrderCard.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Grid } from "@mui/material";
+import AdjustIcon from '@mui/icons-material/Adjust';
+
+const OrderCard = () => {
+    return (
+        <div className="p-5 shadow-lg hover:shadow-xl border rounded">
+            <Grid container spacing={2} sx={{ justifyContent: "space-between" }}>
+                <Grid item xs={6}>
+                    <div className="flex cursor-pointer">
+                        <img
+                            className="w-[5rem] h-[5rem] object-cover object-top"
+                            src="https://rukminim1.flixcart.com/image/612/612/l5h2xe80/kurta/x/6/n/xl-kast-tile-green-majestic-man-original-imagg4z33hu4kzpv.jpeg?q=70"
+                            alt="Men Printed Pure Cotton Straight Kurta"
+                        />
+                        <div className="ml-5 space-y-2">
+                            <p>Men Printed Pure Cotton Straight Kurta</p>
+                            <p className="opacity-50 text-xs font-semibold">Size: M</p>
+                            <p className="opacity-50 text-xs font-semibold">Color: Black</p>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={2}>
+                    <p>₹1099</p>
+                </Grid>
+                <Grid item xs={4}>
+                    {true && <div>
+                        <p>
+                            <AdjustIcon
+                                className="mr-2 text-green-600 text-sm"
+                                sx={{
+                                    width: "15px",
+                                    height: "15px",
+                                }}
+                            />
+                            <span>Delivered On March 03</span>
+                        </p>
+                        <p className="text-xs">Your Item Has Been Delivered</p>
+                    </div>}
+                    {false && <p>
+                        <span>Expected Delivery On Mar 03</span>
+                    </p>}
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
+
+export default OrderCard
